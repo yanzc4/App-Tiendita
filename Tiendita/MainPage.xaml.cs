9d46ae08ee7008ab4ec847876e887a565d05aa78
Yan ZC <yanzapatacardoza@gmail.com>
2023-03-27 15:22:56 -0500

    Agregar archivos de proyecto.

---
+++ Tiendita/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tiendita
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
+        }
+
+        private void btnRegistrarse_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registrarse());
+        }
+    }
+}
